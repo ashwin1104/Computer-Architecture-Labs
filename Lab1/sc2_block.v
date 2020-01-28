c0b544180e5bc4c278a27f263b12ecd5ca14f12a
Ashwin Saxena <ashwins2@siebl-0218-08.ews.illinois.edu>
2020-01-28 13:32:42 -0600

    Completed implementation of sc2_block module based on sc_block.

--- Lab1/sc2_block.v
+++ Lab1/sc2_block.v
@@ -1,7 +1,13 @@
-// Complete the sc2_block module in this file
-// Don't put any code in this file besides the sc2_block
-// Refer to sc_block.v for hints!
-
 module sc2_block(s, cout, a, b, cin);
+    output s, cout;
+    input a, b, cin;
+    wire w1, w2, w3;
+
+    // using module sc_block
+    sc_block sc0(w1, w2, a, b);
+    sc_block sc1(s, w3, w1, cin);
+
+    // "cout" output is the OR of the two sc_block outputs w3 and w2
+    or o1(cout, w3, w2);
 
 endmodule // sc2_block
