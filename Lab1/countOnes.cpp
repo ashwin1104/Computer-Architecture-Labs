865b0153c28603c3f504fb1526bc838417cc663d
ashwin1104 <ashwin.saxena1@gmail.com>
2020-01-30 16:32:53 -0600

    Programmed the algorithm for countOnes and ran testing

--- Lab1/countOnes.cpp
+++ Lab1/countOnes.cpp
@@ -4,9 +4,26 @@
  */
 
 unsigned countOnes(unsigned input) {
-	// TODO: write your code here
+	int right, left, sum;
 
-	
+	right = (input & 0x55555555);
+	left = ((input & 0xAAAAAAAA) >> 1);
+	sum = right + left;
 
-	return input;
+	right = (sum & 0x33333333);
+	left = ((sum & 0xCCCCCCCC) >> 2);
+	sum = right + left;
+
+	right = (sum & 0x0F0F0F0F);
+	left = ((sum & 0xF0F0F0F0) >> 4);
+	sum = right + left;
+
+	right = (sum & 0x00FF00FF);
+	left = ((sum & 0xFF00FF00) >> 8);
+	sum = right + left;
+
+	right = (sum & 0x0000FFFF);
+	left = ((sum & 0xFFFF0000) >> 16);
+
+	return (right + left);
 }
