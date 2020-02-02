186c6bda0288f1e9c97486fdc60772e95588dab3
ashwin1104 <ashwin.saxena1@gmail.com>
2020-02-02 17:46:14 -0600

    Fixed style

--- Lab2/logicunit.v
+++ Lab2/logicunit.v
@@ -5,12 +5,14 @@ module logicunit(out, A, B, control);
     input [1:0] control;
     wire        wAnd, wOr, wNor, wXor, AandB, AorB, AnorB, AxorB, not_control_one, not_control_zero;
 
-    not n1(not_control_zero, control[0]);
-    not n2(not_control_one, control[1]);
     and a1(AandB, A, B);
     or o1(AorB, A, B);
     nor no1(AnorB, A, B);
     xor x1(AxorB, A, B);
+
+    not n1(not_control_zero, control[0]);
+    not n2(not_control_one, control[1]);
+
     and a1(wAnd, AandB, not_control_one, not_control_zero);
     and a2(wOr, AorB, not_control_one, control[0]);
     and a3(wNor, AnorB, control[1], not_control_zero);
