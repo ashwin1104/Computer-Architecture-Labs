f6e6ea6b5e6a82d2ea396b0b521645336e1deac6
ashwin1104 <ashwin.saxena1@gmail.com>
2020-02-02 22:19:35 -0600

    Fixed formatting and added carryout wire

--- Lab2/alu1_tb.v
+++ Lab2/alu1_tb.v
@@ -1,34 +1,35 @@
 module alu1_test;
-  // cycle through all combinations of A, B, and carryin every 8 time units
-  reg A = 0;
-  always #1 A = !A;
-  reg B = 0;
-  always #2 B = !B;
-  reg carryin = 0;
-  always #3 carryin = !carryin;
 
-reg [2:0] control = 0;
+    // cycle through all combinations of A, B, and carryin every 8 time units
+    reg A = 0;
+    always #1 A = !A;
+    reg B = 0;
+    always #2 B = !B;
+    reg carryin = 0;
+    always #3 carryin = !carryin;
 
-initial begin
-    $dumpfile("alu1.vcd");
-    $dumpvars(0, alu1_test);
+    reg [2:0] control = 0;
 
-    // control is initially 0
-    # 4 control = 1; // wait 4 time units and then set it to 1
-    # 4 control = 2; // wait 4 time units and then set it to 2
-    # 4 control = 3; // wait 4 time units and then set it to 3
-    # 4 control = 4; // wait 4 time units and then set it to 4
-    # 4 control = 5; // wait 4 time units and then set it to 5
-    # 4 control = 6; // wait 4 time units and then set it to 6
-    # 4 control = 7; // wait 4 time units and then set it to 7
-    # 4 $finish; // wait 4 time units and then end the simulation
-end
+    initial begin
+      $dumpfile("alu1.vcd");
+      $dumpvars(0, alu1_test);
 
-wire out;
-alu1 a1(out, carryout, A, B, carryin, control);
+      // control is initially 0
+      # 4 control = 1; // wait 4 time units and then set it to 1
+      # 4 control = 2; // wait 4 time units and then set it to 2
+      # 4 control = 3; // wait 4 time units and then set it to 3
+      # 4 control = 4; // wait 4 time units and then set it to 4
+      # 4 control = 5; // wait 4 time units and then set it to 5
+      # 4 control = 6; // wait 4 time units and then set it to 6
+      # 4 control = 7; // wait 4 time units and then set it to 7
+      # 4 $finish; // wait 4 time units and then end the simulation
+    end
 
-initial begin
-    $display("A B cin c o cout");
-    $monitor("%d %d %d %d %d %d (at time %t)", A, B, carryin, control, out, carryout, $time);
-end
+    wire out, carryout;
+    alu1 a1(out, carryout, A, B, carryin, control);
+
+    initial begin
+      $display("A B cin c o cout");
+      $monitor("%d %d %d %d %d %d (at time %t)", A, B, carryin, control, out, carryout, $time);
+    end
 endmodule // alu1_test
