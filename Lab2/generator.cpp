add038f94e934b9f5ac665389471667504b61c95
Ashwin Saxena <ashwins2@siebl-0218-05.ews.illinois.edu>
2020-02-04 13:30:48 -0600

    Created generator for alu1 module calls in alu32 verilog

--- Lab2/generator.cpp
+++ Lab2/generator.cpp
@@ -1,5 +1,9 @@
-// a code generator for the ALU chain in the 32-bit ALU
-// look at example_generator.cpp for inspiration
+#include <cstdio>
+using std::printf;
 
-// make generator
-// ./generator
+int main() {
+    int width = 32;
+    for (int i = 2 ; i < width ; i ++) {
+        printf("alu1 a%d(out[%d], c[%d], A[%d], B[%d], c[%d], control);\n", i, i, i, i, i, i-1);
+    }
+}
