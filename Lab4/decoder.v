49e706a2c710e2155b0d9bdddfd3204b39bd6202
Ashwin Saxena <ashwins2@siebl-0218-26.ews.illinois.edu>
2020-02-11 15:08:38 -0600

    Implemented decoder and created test bench

--- Lab4/decoder.v
+++ Lab4/decoder.v
@@ -15,4 +15,34 @@ module mips_decode(rd_src, writeenable, alu_src2, alu_op, except, opcode, funct)
     output [2:0] alu_op;
     input  [5:0] opcode, funct;
 
+    wire  op_addu = ((opcode == `OP_OTHER0) && (funct == `OP0_ADDU));
+    wire  op_addiu = opcode == `OP_ADDIU;
+
+    wire  op_add = (opcode == `OP_OTHER0) && (funct == `OP0_ADD);
+    wire  op_addi = opcode == `OP_ADDI;
+
+    wire  op_and = (opcode == `OP_OTHER0) && (funct == `OP0_AND);
+    wire  op_andi = opcode == `OP_ANDI;
+
+    wire  op_or = (opcode == `OP_OTHER0) && (funct == `OP0_OR);
+    wire  op_ori = opcode == `OP_ORI;
+
+    wire  op_xor = (opcode == `OP_OTHER0) && (funct == `OP0_XOR);
+    wire  op_xori = opcode == `OP_XORI;
+
+    wire  op_nor = (opcode == `OP_OTHER0) && (funct == `OP0_NOR);
+    wire  op_sub = (opcode == `OP_OTHER0) && (funct == `OP0_SUB);
+
+    assign writeenable = op_addu | op_addiu | op_add | op_addi | op_and | op_andi | op_or | op_ori | op_xor | op_xori | op_nor | op_sub;
+    assign except = ~writeenable;
+
+    assign alu_op[0] = op_sub | op_or | op_ori | op_xor | op_xori;
+    assign alu_op[1] = op_add | op_addi | op_sub | op_nor | op_xor | op_xori;
+    assign alu_op[2] = op_and | op_andi | op_or | op_ori | op_nor | op_xor | op_xori;
+
+    assign alu_src2[0] = op_addi | op_addiu;
+    assign alu_src2[1] = op_andi | op_ori | op_xori;
+
+    assign rd_src = op_addi | op_addiu | op_andi | op_ori | op_xori;
+
 endmodule // mips_decode
--- Lab4/decoder_tb.v
+++ Lab4/decoder_tb.v
@@ -8,13 +8,23 @@ module decoder_test;
              opcode = `OP_OTHER0; funct = `OP0_ADD; // try addition
         # 10 opcode = `OP_OTHER0; funct = `OP0_SUB; // try subtraction
         // add more tests here!
-
+        # 10 opcode = `OP_OTHER0; funct = `OP0_ADDU;
+        # 10 opcode = `OP_OTHER0; funct = `OP0_AND;
+        # 10 opcode = `OP_OTHER0; funct = `OP0_OR;
+        # 10 opcode = `OP_OTHER0; funct = `OP0_XOR;
+        # 10 opcode = `OP_OTHER0; funct = `OP0_NOR;
+        # 10 opcode = `OP_ADDIU;
+        # 10 opcode = `OP_ADDI;
+        # 10 opcode = `OP_ANDI;
+        # 10 opcode = `OP_ORI;
+        # 10 opcode = `OP_XORI;
+        # 10 opcode = `OP_SWR;
         # 10 $finish;
     end
 
     // use gtkwave to test correctness
     wire [2:0] alu_op;
-    wire [1:0] alu_src2; 
+    wire [1:0] alu_src2;
     wire       rd_src, writeenable, except;
     mips_decode decoder(rd_src, writeenable, alu_src2, alu_op, except,
                         opcode, funct);
