69e49e7351b1e181afdd11392d1fc4c78d710d4b
Ashwin Saxena <ashwins2@siebl-0218-26.ews.illinois.edu>
2020-02-25 17:07:46 -0600

    Finished 2.1

--- Lab6/p2.s
+++ Lab6/p2.s
@@ -6,7 +6,7 @@
 #             output[i*width + j] = 0;
 #             for (int k = 0; k < width; k++) {
 #                 output[i*width + j] += matr_a[i*width + k] * matr_b[k*width + j];
-#             }
+#             }i*width + j
 #         }
 #     }
 # }
@@ -18,6 +18,74 @@
 
 .globl matrix_mult
 matrix_mult:
+	li $t0 0			# i = 0
+	li $t1 0			# j = 0
+	li $t2 0			# k = 0
+	li $t3 0			# 4(i*width + j) = 0
+	li $t4 0			# 4(i*width + k) = 0
+	li $t5 0			# 4(k*width + j) = 0
+	li $t6 0			# matr_a*martr_b = 0
+	li $t7 0			# $t6 + $t3
+
+for_1:
+	bge $t0, $a3, end			# !(i < width)
+	j for_2
+
+for_2:
+	bge $t1, $a3, for_1_inc			# !(j < width)
+
+	mul $t3, $t0, $a3						# t3 = i*width
+	add $t3, $t3, $t1						# t3 = i*width + j
+	add $t3, $t3, $t3						# t3 = 2*t3
+	add $t3, $t3, $t3						# t3 = 2*t3 = 4(i*width+j)
+
+	add $t3, $t3, $a2						# t3 = &output[0] + t3
+
+	sw $0, 0($t3)										# output[i*width+j] = 0
+
+	j for_3
+
+for_3:
+	bge $t2, $a3, for_2_inc			# !(k < width)
+
+	mul $t4, $t0, $a3						# t4 = i*width
+	add $t4, $t4, $t2						# t4 = t4 + k = i*width + k
+	add $t4, $t4, $t4						# t4 *= 2
+	add $t4, $t4, $t4						# t4 = 2*t4 = 4(i*width + k)
+	add $t4, $t4, $a0						# t4 = &matr_a[0] + t4
+
+	mul $t5, $t2, $a3						# t5 = k*width
+	add $t5, $t5, $t1						# t5 = t4 + j = k*width + j
+	add $t5, $t5, $t5						# t5 *= 2
+	add $t5, $t5, $t5						# t5 = 2*t5 = 4(k*width + j)
+	add $t5, $t5, $a1						# t5 = &matr_b[0] + t5
+
+	lw $t6, 0($t4)
+	lw $t7, 0($t5)
+
+	mul $t6, $t6, $t7						# t6 = matr_a[i*width+k]*matr_b[k*width + j]
+	lw $t7, 0($t3)
+
+	add $t6, $t6, $t7						# t7 = t6 + output[i*width+j]
+	sw $t6, 0($t3)							# output[i*width+j] = t7
+
+	j for_3_inc
+
+for_1_inc:
+	li $t1 0			# j = 0
+	addi	$t0, $t0, 1			# i += 1
+	j	for_1				# jump to for_1
+
+for_2_inc:
+	li $t2 0			# k = 0
+	addi $t1, $t1, 1			# j += 1
+	j for_2
+
+for_3_inc:
+	addi $t2 $t2 1			# k += 1
+	j for_3
+
+end:
 	jr	$ra
 
 
@@ -40,4 +108,3 @@ matrix_mult:
 markov_chain:
 	# Can access working_matrix from p2_main.s
 	jr	$ra
-	
