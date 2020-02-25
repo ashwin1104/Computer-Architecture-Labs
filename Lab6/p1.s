19f2969c08a5126cb355dec2519a7436673a343d
Ashwin Saxena <ashwins2@siebl-0218-26.ews.illinois.edu>
2020-02-25 14:59:14 -0600

    Finish part 1

--- Lab6/p1.s
+++ Lab6/p1.s
@@ -21,4 +21,32 @@
 
 .globl update_alert_level
 update_alert_level:
-	jr	$ra
+	li $t0, 0		# total_monster = 0
+	li $t1, 0		# i = 0
+	li $t2, 10	# for loop end = 10
+
+update_alert_level_for:
+	bge $t1, $t2, update_alert_level_if # !(i < for loop end)
+	sll	$t3, $t1, 2 # i *= 4
+	add	$t3, $a0, $t3		# $t3 = &stockpiles[i]
+	lw	$t3, 0($t3)		# $t3 = stockpiles[i]
+	add $t0, $t0, $t3 # total_monster = total_monster + stockpiles[i]
+
+update_alert_level_for_inc:
+		addi	$t1, $t1, 1
+		j	update_alert_level_for
+
+update_alert_level_if:
+		bge $t0, $a1, update_alert_level_if_2
+		addi $v0, $a2, 1
+		jr $ra
+
+update_alert_level_if_2:
+		bne $t0, $a1, update_alert_level_if_3
+		move $v0, $a2
+		jr $ra
+
+update_alert_level_if_3:
+		li $t4, 1
+		sub $v0, $a2, $t4
+		jr $ra
