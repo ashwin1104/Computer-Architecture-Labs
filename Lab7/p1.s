71bd9b762063d8190ee65999473007c1014c1181
ashwin1104 <ashwin.saxena1@gmail.com>
2020-03-05 18:42:50 -0600

    fix

--- Lab7/p1.s
+++ Lab7/p1.s
@@ -1,7 +1,7 @@
 .text
 
 # // part 1 p1.s
-# unsigned char find_payment(TreeNode* trav) { 
+# unsigned char find_payment(TreeNode* trav) {
 # 	// Base case
 # 	if (trav == NULL) {
 # 		return 0;
@@ -16,5 +16,59 @@
 
 .globl find_payment
 find_payment:
-	li $v0, 0
+	beq $a0, $zero, null_check			# if trav is null go to null_check
+	jal setup_payment
+
+setup_payment:
+	sub $sp, $sp, 32								# allocate memory necessary with stack pointer
+																	# store all necessary save registers
+	sw $ra, 0($sp)
+	sw $s0, 4($sp)
+	move $s0, $a0										# let s0 store contents of trav
+	sw $s1, 8($sp)
+	sw $s2, 12($sp)
+	sw $s3, 16($sp)
+	sw $s4, 20($sp)
+	sw $s5, 24($sp)
+
+	jal start_payment
+
+start_payment:
+	li $s5, 0												# set future v0 to 0
+
+	lw $s1, 0($s0)									# store trav->left into s1
+	move $a0, $s1										# store trav->left into argument
+	jal find_payment								# recursive call with trav->left
+
+	add $s5, $s5, $v0								# add return value from above call to s5
+	lw $s2, 4($s0)									# store trav->current into s2
+	move $a0, $s2										# store trav->current into argument
+	jal find_payment								# recursive call with trav->current
+
+	add $s5, $s5, $v0								# same thing
+	lw $s3, 8($s0)
+	move $a0, $s3
+	jal find_payment
+
+	add $s5, $s5, $v0
+	lb $s4, 12($s0)									# store trav->value into s4
+	add $s5, $s5, $s4								# add trav->value to s5
+
+	div $s5, $s5, 2									# divide s5 by 2
+	move $v0, $s5										# store s5 (ans) into v0 (return register)
+	jal finish_payment
+
+finish_payment:
+	lw $ra, 0($sp)									# reallocate memory by loading back every register
+	lw $s0, 4($sp)
+	lw $s1, 8($sp)
+	lw $s2, 12($sp)
+	lw $s3, 16($sp)
+	lw $s4, 20($sp)
+	lw $s5, 24($sp)
+	addi $sp, $sp, 32
 	jr $ra
+
+null_check:
+	li $v0, 0
+	jr $ra				# return 0 and go to return address
