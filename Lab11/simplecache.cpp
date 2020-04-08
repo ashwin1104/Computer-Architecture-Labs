2099b3e6564f94bb636bc8fd0dcdf6666a042a80
ashwin1104 <ashwin.saxena1@gmail.com>
2020-04-08 14:13:58 -0500

    Finish pt 1

--- Lab11/simplecache.cpp
+++ Lab11/simplecache.cpp
@@ -1,11 +1,31 @@
 #include "simplecache.h"
 
-int SimpleCache::find(int index, int tag, int block_offset) {
-  // read handout for implementation details
+int SimpleCache::find(int index, int tag, int block_offset)
+{
+  for (SimpleCacheBlock b : _cache[index])
+  {
+    if (b.valid() && b.tag() == tag)
+    {
+      return b.get_byte(block_offset);
+    }
+  }
   return 0xdeadbeef;
 }
 
-void SimpleCache::insert(int index, int tag, char data[]) {
-  // read handout for implementation details
-  // keep in mind what happens when you assign (see "C++ Rule of Three")
+/*For insert, you will again look up the set of blocks corresponding to the given index. Next, look through
+the set for an invalid block. If there is one, replace it with the block being inserted and return. If not,
+replace the 0th block of the set (overwriting whatever was in it). Use the provided replace function in
+simplecache.h. */
+
+void SimpleCache::insert(int index, int tag, char data[])
+{
+  for (SimpleCacheBlock &b : _cache[index])
+  {
+    if (!b.valid())
+    {
+      b.replace(tag, data);
+      return;
+    }
+  }
+  _cache[index][0].replace(tag, data);
 }
