5507f6112a740ea94856a3afd0c94babbab3496e
ashwin1104 <ashwin.saxena1@gmail.com>
2020-04-19 19:26:57 -0500

    part 2

--- Lab12/filter.cpp
+++ Lab12/filter.cpp
@@ -3,49 +3,64 @@
 #include "filter.h"
 
 // modify this code by fusing loops together
-void
-filter_fusion(pixel_t **image1, pixel_t **image2) {
-    for (int i = 1; i < SIZE - 1; i ++) {
+void filter_fusion(pixel_t **image1, pixel_t **image2)
+{
+    for (int i = 1; i < SIZE - 1; i++)
+    {
         filter1(image1, image2, i);
+        if (i >= 2 && i < SIZE - 2)
+        {
+            filter2(image1, image2, i);
+        }
+        if (i > 5)
+        {
+            filter3(image2, i - 5);
+        }
     }
-
-    for (int i = 2; i < SIZE - 2; i ++) {
-        filter2(image1, image2, i);
-    }
-
-    for (int i = 1; i < SIZE - 5; i ++) {
-        filter3(image2, i);
-    }
+    filter3(image2, SIZE - 6);
 }
 
 // modify this code by adding software prefetching
-void
-filter_prefetch(pixel_t **image1, pixel_t **image2) {
-    for (int i = 1; i < SIZE - 1; i ++) {
+void filter_prefetch(pixel_t **image1, pixel_t **image2)
+{
+    for (int i = 1; i < SIZE - 1; i++)
+    {
+        __builtin_prefetch(image1[i + 15], 0, 3);
+        __builtin_prefetch(image2[i + 15], 1, 0);
         filter1(image1, image2, i);
     }
 
-    for (int i = 2; i < SIZE - 2; i ++) {
+    for (int i = 2; i < SIZE - 2; i++)
+    {
+        __builtin_prefetch(image1[i + 11], 0, 3);
+        __builtin_prefetch(image2[i + 11], 1, 0);
         filter2(image1, image2, i);
     }
 
-    for (int i = 1; i < SIZE - 5; i ++) {
+    for (int i = 1; i < SIZE - 5; i++)
+    {
+        __builtin_prefetch(image1[i + 13], 0, 1);
+        __builtin_prefetch(image2[i + 13], 1, 0);
         filter3(image2, i);
     }
 }
 
 // modify this code by adding software prefetching and fusing loops together
-void
-filter_all(pixel_t **image1, pixel_t **image2) {
-    for (int i = 1; i < SIZE - 1; i ++) {
+void filter_all(pixel_t **image1, pixel_t **image2)
+{
+    for (int i = 1; i < SIZE - 1; i++)
+    {
+        __builtin_prefetch(image1[i + 7], 0, 1);
+        __builtin_prefetch(image2[i + 7], 1, 0);
         filter1(image1, image2, i);
+        if (i >= 2 && i < SIZE - 2)
+        {
+            filter2(image1, image2, i);
+        }
+        if (i > 5)
+        {
+            filter3(image2, i - 5);
+        }
     }
-
-    for (int i = 2; i < SIZE - 2; i ++) {
-        filter2(image1, image2, i);
-    }
-
-    for (int i = 1; i < SIZE - 5; i ++) {
-        filter3(image2, i);
-    }
+    filter3(image2, SIZE - 6);
 }
